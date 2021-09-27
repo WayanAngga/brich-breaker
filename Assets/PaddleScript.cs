083886000d1c2b5185efa7377b42665b5399d6ec
WayanAngga <54860700+WayanAngga@users.noreply.github.com>
2021-09-27 11:20:11 +0700

    Add files via upload

---
+++ Assets/PaddleScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed;
+    public float maxX;
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update(){
+        if (gm.gameOver){
+            return;
+        }
+        float x = Input.GetAxis("Horizontal");
+
+        if (x < 0)
+        {
+            MoveLeft();
+        }
+
+        if (x > 0)
+        {
+            MoveRight();
+        }
+
+        if (x == 0)
+        {
+            Stop();
+        }
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp (pos.x,-maxX,maxX);
+        transform.position = pos;
+
+        
+    }
+
+    void MoveLeft(){
+        rb.velocity = new Vector2 (-speed, 0);
+    }
+
+    void MoveRight(){
+        rb.velocity = new Vector2 (speed, 0);
+    }
+
+    void Stop(){
+        rb.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void QuitGame(){
+        Application.Quit();
+        Debug.Log ("Quit Button Pushed");
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("level1");
+    }
+}
